9c94f562f59112a9bc2148d3be85fd54fbebe7cf
DheerajRekula <47189595+DheerajRekula@users.noreply.github.com>
2019-06-14 08:32:21 -0400

    update user input validation. Also added __Main__
    defined a function for user input validation. also added __Name__ = __Main__

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,6 +1,8 @@
 # shopping_cart.py
 
 import datetime
+import os
+import json
 
 
 products = [
@@ -33,7 +35,14 @@ products = [
 
 ###########################
 # CODE
+'''
+c_var = os.path.join(os.path.dirname(__file__), "..", "data", "common_variable.json")
 
+#with open(c_var) as json_file:  
+ #   data = json.load(json_file)
+
+breakpoint()
+'''
 # rounds the number to 2 decimal digits and adds $ sign to indicate currency
 def to_usd(dollar_amt):
     return "${0:,.2f}".format(dollar_amt)
@@ -53,53 +62,98 @@ def def_date():
     now = datetime.datetime.now()
     return now.strftime("%Y-%m-%d:%I:%M:%S %p")
 
-# STEP 1: TAKE INPUTS AND PRINT THEM
-selected_products = []
-total_price = 0
-product_ids = [i["id"] for i in products]    # list of all products available. Useful for validation purposes
+def validate_prod(prod, available_products = []):
+    #available_products = [p["id"] for p in products]
+    
+    if str(prod) in str(available_products) or prod.lower() == "done":
+        return str(prod).lower()
+#
+#
+#    else:
+#        error_msg = "You have entered an invalid product id. Enter again or contact the store manager"
+#        return error_msg
+#         #print("You have entered an invalid product id. Enter again or contact the store manager")
 
-while True:
-    input_id = input("Please input a product identifier:")
 
-    # Validate user input
-    if str(input_id) in str(product_ids) or input_id.lower() == "done":
-        
+'''
+    mapped_prod = [p["id"] for p in products if str(p["id"]) == str(prod)]
+    return mapped_prod
+'''
+
+# execute the code only when the program is invoked from CLI.
+if __name__ == "__main__":
+    
+    # STEP 1: TAKE INPUTS AND PRINT THEM
+    selected_products = []
+    total_price = 0
+    product_ids = [i["id"] for i in products]    # list map. list of all products available. Useful for validation purposes
+
+
+    while True:
+        input_id = input("Please input a product identifier:")
+
+        mapped_prod = validate_prod(input_id, product_ids)
+        #breakpoint()
+
+        #mapped_prod = [list_prod]
+        #breakpoint()
+
         # If the input is valid, do further processing until user enters Done
         # Done signal is case agnostic
-        if input_id.lower() == "done":
-            break
+        if mapped_prod != None:
+            
+            if mapped_prod == "done":
+                break
+            else:
+                selected_products.append(mapped_prod)
+        
         else:
-            selected_products.append(input_id)
-    else:
-        print("You have entered an invalid product id. Enter again or contact the store manager")
-    
-print("-------------------------------------------------")
-print("GREEN FOODS GROCERY STORE")       #TODO: This is in main script. Can i put it in a different script
-print("WWW.GREEN-FOODS-GROCERY.COM")     #TODO: This is in main script. Can i put it in a different script. Can i make this a hyperlink
-print("-------------------------------------------------")
-print(f"CHECK OUT AT: {def_date()}")
-print("-------------------------------------------------")
-print("SELECTED PRODUCTS")
+            print("You have entered an invalid product id. Enter again or contact the store manager")
+  
+    ## this code is working. if function fails, restore this code
+    #while True:
+    #    input_id = input("Please input a product identifier:")
+#
+    #    # Validate user input
+    #    if str(input_id) in str(product_ids) or input_id.lower() == "done":
+#
+    #        # If the input is valid, do further processing until user enters Done
+    #        # Done signal is case agnostic
+    #        if input_id.lower() == "done":
+    #            break
+    #        else:
+    #            selected_products.append(input_id)
+    #    else:
+    #        print("You have entered an invalid product id. Enter again or contact the store manager")
 
 
-for selected_product in selected_products:
-    product_match = [p for p in products if str(p["id"]) in str(selected_product)]
-    total_price = total_price + product_match[0]['price']
-    product_price = to_usd(product_match[0]['price'])
-    print(f"... {product_match[0]['name']} ({product_price})")
+    print("-------------------------------------------------")
+    print("GREEN FOODS GROCERY STORE")       #TODO: This is in main script. Can i put it in a different script
+    print("WWW.GREEN-FOODS-GROCERY.COM")     #TODO: This is in main script. Can i put it in a different script. Can i make this a hyperlink
+    print("-------------------------------------------------")
+    print(f"CHECK OUT AT: {def_date()}")
+    print("-------------------------------------------------")
+    print("SELECTED PRODUCTS")
 
-print("-------------------------------------------------")
+
+    for selected_product in selected_products:
+        product_match = [p for p in products if str(p["id"]) in str(selected_product)]
+        total_price = total_price + product_match[0]['price']
+        product_price = to_usd(product_match[0]['price'])
+        print(f"... {product_match[0]['name']} ({product_price})")
+
+    print("-------------------------------------------------")
 
 
-#STEP 2: PRINT SUBTOTAL AND GRAND TOTAL
-product_tot_amt = to_usd(total_price)
-product_tot_tax = to_usd(calc_tax(total_price))
-product_grand_tot = to_usd(calc_total_bill_amt(total_price))
-print(f"SUBTOTAL = {product_tot_amt}")
-print(f"TAX = {product_tot_tax}")
-print(f"TOTAL = {product_grand_tot}")
-print("-------------------------------------------------")
-print("Thanks, SEE YOU AGAIN SOON!")
-print("-------------------------------------------------")
+    #STEP 2: PRINT SUBTOTAL AND GRAND TOTAL
+    product_tot_amt = to_usd(total_price)
+    product_tot_tax = to_usd(calc_tax(total_price))
+    product_grand_tot = to_usd(calc_total_bill_amt(total_price))
+    print(f"SUBTOTAL = {product_tot_amt}")
+    print(f"TAX = {product_tot_tax}")
+    print(f"TOTAL = {product_grand_tot}")
+    print("-------------------------------------------------")
+    print("Thanks, SEE YOU AGAIN SOON!")
+    print("-------------------------------------------------")
 
 
