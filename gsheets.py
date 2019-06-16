d9e3043e1f3fd92bcf3900f6b03720414b293b81
DheerajRekula <47189595+DheerajRekula@users.noreply.github.com>
2019-06-16 06:12:29 -0400

    Handled leading zeroes, and cleaned up the code
    removed unecessary comments and breakpoints

--- gsheets.py
+++ gsheets.py
@@ -1,6 +1,5 @@
-#
-## SOURCE: https://techwithtim.net/tutorials/google-sheets-python-api-tutorial/
-#
+# gsheets.py
+
 from dotenv import load_dotenv
 import os
 import gspread
@@ -30,36 +29,16 @@ def google_sheets_data():
 
     return data
 
-#breakpoint()
 
-#print("-----------------")
-#print("SPREADSHEET:", doc.title)
-#print("-----------------")
-
-''' #working  code
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-
-creds = ServiceAccountCredentials.from_json_keyfile_name("gcreds.json", scope)
-
-client = gspread.authorize(creds)
-
-sheet = client.open("shopping_cart").sheet1  # Open the spreadhseet
-
-data = sheet.get_all_records()  # Get a list of all records
-pprint(data)
-'''
-
-
-
-'''
-row = sheet.row_values(3)  # Get a specific row
-col = sheet.col_values(3)  # Get a specific column
-cell = sheet.cell(1,2).value  # Get the value of a specific cell
-
-insertRow = ["hello", 5, "red", "blue"]
-sheet.add_rows(insertRow, 4)  # Insert the list as a row at index 4
-
-sheet.update_cell(2,2, "CHANGED")  # Update one cell
-
-numRows = sheet.row_count  # Get the number of rows in the sheet
+''' #working  code. Ignore this for now
+ scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+ 
+ creds = ServiceAccountCredentials.from_json_keyfile_name("gcreds.json", scope)
+ 
+ client = gspread.authorize(creds)
+ 
+ sheet = client.open("shopping_cart").sheet1  # Open the spreadhseet
+ 
+ data = sheet.get_all_records()  # Get a list of all records
+ pprint(data)
 '''
--- shopping_cart.py
+++ shopping_cart.py
@@ -8,37 +8,9 @@ import pprint
 
 products = google_sheets_data()
 
-'''
-for product in products:
-    print(product)
+#breakpoint()
 
-breakpoint()
-'''
-
-'''
-products = [
-    {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
-    {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
-    {"id":3, "name": "Robust Golden Unsweetened Oolong Tea", "department": "beverages", "aisle": "tea", "price": 2.49},
-    {"id":4, "name": "Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce", "department": "frozen", "aisle": "frozen meals", "price": 6.99},
-    {"id":5, "name": "Green Chile Anytime Sauce", "department": "pantry", "aisle": "marinades meat preparation", "price": 7.99},
-    {"id":6, "name": "Dry Nose Oil", "department": "personal care", "aisle": "cold flu allergy", "price": 21.99},
-    {"id":7, "name": "Pure Coconut Water With Orange", "department": "beverages", "aisle": "juice nectars", "price": 3.50},
-    {"id":8, "name": "Cut Russet Potatoes Steam N' Mash", "department": "frozen", "aisle": "frozen produce", "price": 4.25},
-    {"id":9, "name": "Light Strawberry Blueberry Yogurt", "department": "dairy eggs", "aisle": "yogurt", "price": 6.50},
-    {"id":10, "name": "Sparkling Orange Juice & Prickly Pear Beverage", "department": "beverages", "aisle": "water seltzer sparkling water", "price": 2.99},
-    {"id":11, "name": "Peach Mango Juice", "department": "beverages", "aisle": "refrigerated", "price": 1.99},
-    {"id":12, "name": "Chocolate Fudge Layer Cake", "department": "frozen", "aisle": "frozen dessert", "price": 18.50},
-    {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
-    {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
-    {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
-    {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
-    {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
-    {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
-    {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
-] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
-'''
+############################# HARDCODED DATASET
 '''
 products = [
     {"id":1, "price_per": "item", "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -68,20 +40,9 @@ products = [
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 '''
 
-# prompt the checkout clerk to input the identifier of each shopping cart item, one at a time. 
-# When the clerk enters DONE, process payment
-# If product id is not found, ask the user to add a correct entry
 
-###########################
-# CODE
-'''
-c_var = os.path.join(os.path.dirname(__file__), "..", "data", "common_variable.json")
+########################### FUNCTIONS
 
-#with open(c_var) as json_file:  
- #   data = json.load(json_file)
-
-breakpoint()
-'''
 # rounds the number to 2 decimal digits and adds $ sign to indicate currency
 def to_usd(dollar_amt):
     return "${0:,.2f}".format(dollar_amt)
@@ -103,30 +64,16 @@ def def_date():
 
 def def_date_text():
     now = datetime.datetime.now()
-    #return now
     return now.strftime("%Y-%m-%d-%I-%M-%S-%f %p")
 
 def validate_prod(prod, available_products = []):
-    #available_products = [p["id"] for p in products]
-    
-    if str(prod) in str(available_products) or prod.lower() == "done":
-        return str(prod).lower()
+    #for loop is important to handle 0 entry
+    for available_product in available_products:           
+        if str(prod) == str(available_product) or prod.lower() == "done":
+            return str(prod).lower()
 
 
-#
-#
-#    else:
-#        error_msg = "You have entered an invalid product id. Enter again or contact the store manager"
-#        return error_msg
-#         #print("You have entered an invalid product id. Enter again or contact the store manager")
-
-
-'''
-    mapped_prod = [p["id"] for p in products if str(p["id"]) == str(prod)]
-    return mapped_prod
-'''
-
-# execute the code only when the program is invoked from CLI.
+# EXECUTES THE BELOW ONLY WHEN THE PROGRAM IS INVOKED FROM CLI. IGNORED FOR PYTESTS
 if __name__ == "__main__":
     #breakpoint()
     
@@ -134,7 +81,8 @@ if __name__ == "__main__":
     selected_products_item = []
     selected_products_pound = []
     total_price = 0
-    #product_ids = [i["id"] for i in products]    # type : LIST, INT. list of all products available. Useful for validation purposes
+    
+    # type : LIST, INT. list of all products available. Useful for validation purposes
     product_ids_item = [str(i["id"]) for i in products if i["price_per"] == 'item']
     product_ids_pound = [str(i["id"]) for i in products if i["price_per"] == "pound"]
     consolidate_products_ids = product_ids_item + product_ids_pound
@@ -144,14 +92,11 @@ if __name__ == "__main__":
     while True:
         input_id = input("Please input a product identifier:")   #type : STR
 
-        #mapped_prod = validate_prod(input_id, product_ids)
-        
-        #breakpoint()
-        mapped_prod = validate_prod(input_id, consolidate_products_ids)
-        #breakpoint()
+        input_id = input_id.lstrip("0")   # for handling leading zeroes. python is reading data without leading zeroes
 
-        #mapped_prod = [list_prod]
-        #breakpoint()
+        
+        mapped_prod = validate_prod(input_id, consolidate_products_ids)
+        
 
         # If the input is valid, do further processing until user enters Done
         # Done signal is case agnostic
@@ -177,36 +122,10 @@ if __name__ == "__main__":
                 else:
                     selected_products_item.append(mapped_prod)
                     
-
-
-            
-            #if mapped_prod == "done":
-            #    break
-            #else:
-            #    selected_products.append(mapped_prod)
-        
         else:
             print("You have entered an invalid product id. Enter again or contact the store manager")
         
-    #breakpoint()
-  
-    ## this code is working. if function fails, restore this code
-    #while True:
-    #    input_id = input("Please input a product identifier:")
-#
-    #    # Validate user input
-    #    if str(input_id) in str(product_ids) or input_id.lower() == "done":
-#
-    #        # If the input is valid, do further processing until user enters Done
-    #        # Done signal is case agnostic
-    #        if input_id.lower() == "done":
-    #            break
-    #        else:
-    #            selected_products.append(input_id)
-    #    else:
-    #        print("You have entered an invalid product id. Enter again or contact the store manager")
-
-
+    
     print("-------------------------------------------------")
     print("GREEN FOODS GROCERY STORE")       #TODO: This is in main script. Can i put it in a different script
     print("WWW.GREEN-FOODS-GROCERY.COM")     #TODO: This is in main script. Can i put it in a different script. Can i make this a hyperlink
@@ -218,21 +137,14 @@ if __name__ == "__main__":
 
     
     # filters empty strings from the list
-    #selected_products = selected_products.strip()
-
     selected_products_item = [p for p in selected_products_item if p.strip() != '']  # type : LIST, STR
-    #selected_products_item = list(map(int, selected_products_item))
-
-    #selected_products_pound = [p for p in selected_products_pound if p.strip() != '']  # type : LIST, STR
-    #selected_products_pound = list(map(int, selected_products_pound))
-    #breakpoint()
+    # selected_products_item = list(map(int, selected_products_item))
 
     # HANDLE ITEMS
     product_list = []
 
     for selected_product in selected_products_item:
         product_match = [p for p in products if str(p["id"]).lower() == selected_product]
-        #breakpoint()     
         total_price = total_price + product_match[0]['price']
         product_price = to_usd(product_match[0]['price'])
         item = {'name' : product_match[0]['name'], 'price' : product_price}
@@ -247,7 +159,6 @@ if __name__ == "__main__":
         p_id = selected_product["id"]
         p_qty = selected_product["qty"]
         product_match = [p for p in products if str(p["id"]).lower() == p_id]
-        #breakpoint()     
         total_price = total_price + product_match[0]['price'] * p_qty
         product_price = to_usd(product_match[0]['price'] * p_qty)
         item = {'name' : product_match[0]['name'], 'price' : product_price}
@@ -277,8 +188,6 @@ if __name__ == "__main__":
 
     with open(file_name, 'w') as file:
 
-        #file.write(print_header())
-
         file.write("-------------------------------------------------\n")
         file.write("GREEN FOODS GROCERY STORE\n")       #TODO: This is in main script. Can i put it in a different script
         file.write("WWW.GREEN-FOODS-GROCERY.COM\n")     #TODO: This is in main script. Can i put it in a different script. Can i make this a hyperlink
@@ -304,35 +213,4 @@ if __name__ == "__main__":
     #file_name = os.path.join(os.path.dirname(__file__), "..", "receipts", "tt.txt")
 
 
-''' 
-    "my_message.txt" # a relative filepath
-
-    with open(file_name, 'w') as file: # "w" means "open the file for writing" 
-        file.write("Hello World")
-        file.write("\n")
-        file.write("\n")
-        file.write("...")
-        file.write("\n")
-        file.write("\n")
-        file.write("Hello Again")
-
-       # gradebook_filepath = os.path.join(os.path.dirname(__file__), "..", "receipts", "test.txt")
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#TODO: Send email receipt, write email receipt in folder, google sheets input, price and header automation, pound
+#TODO: Send email receipt, google sheets input, price and header automation
